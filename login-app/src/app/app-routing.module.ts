97252e954f8dec771d088cf28287ba040626adad
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-03-09 16:13:00 +0300

    :art: Added routing to app for not founded pages

--- login-app/src/app/app-routing.module.ts
+++ login-app/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "dashboard", component: DashboardComponent }
+  { path: "dashboard", component: DashboardComponent },
+  { path: "**", redirectTo: "login" }
 ];
 
 @NgModule({
